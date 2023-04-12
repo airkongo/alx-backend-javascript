d16af7371932dcbc4506455fb7851696cc92624b
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2023-04-12 11:56:59 +0300

    Update 6-payment_token.js

--- 0x06-unittests_in_js /6-payment_token.js
+++ 0x06-unittests_in_js /6-payment_token.js
@@ -1,5 +1,9 @@
-'use strict';
 function getPaymentTokenFromAPI(success) {
-  if (success) return Promise.resolve({ data: 'Successful response from the API' });
+    if (success) {
+	return new Promise((resolve) => {
+          resolve({data: 'Successful response from the API' });
+	});
+    }
 }
+
 module.exports = getPaymentTokenFromAPI;
