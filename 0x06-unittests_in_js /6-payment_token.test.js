23267de41c05f9db61dea1d44157d012e14b5984
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2023-04-12 11:51:01 +0300

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js /6-payment_token.test.js
+++ 0x06-unittests_in_js /6-payment_token.test.js
@@ -1,14 +1,13 @@
-'use strict';
-const chai = require('chai');
-const getPaymentTokenFromAPI = require('./6-payment_token.js');
+const getPaymentTokenFromAPI = require("./6-payment_token");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-describe('getPaymentTokenFromAPI', () => {
-  it('async tests with done', (done) => {
-    getPaymentTokenFromAPI(true)
-      .then((res) => {
-        chai.expect(res).to.include({ data: 'Successful response from the API' });
-      done();
-      })
-      .catch((error) => done(error));
-  });
+describe("getPaymentTokenFromAPI", function() {
+    it("Async testing with done callback", function(done) {
+	getPaymentTokenFromAPI(true)
+	    .then((data) => {
+		expect(data).to.have.property('data');
+		done();
+	    });
+    });
 });
