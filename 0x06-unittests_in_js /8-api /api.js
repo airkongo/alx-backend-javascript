66896215a965fd7999840227e3f317d0428e370a
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2023-04-12 12:08:57 +0300

    Update api.js

--- 0x06-unittests_in_js /8-api /api.js
+++ 0x06-unittests_in_js /8-api /api.js
@@ -1,7 +1,14 @@
-'use strict';
 const express = require('express');
+
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+    console.log("API available on localhost port 7865");
+});
+
+module.exports = app;
