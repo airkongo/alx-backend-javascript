fd201e8dedc1708dcbd0075a5d8de1d0d4404b2e
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2023-04-12 12:05:33 +0300

    Update api.test.js

--- 0x06-unittests_in_js /9-api /api.test.js
+++ 0x06-unittests_in_js /9-api /api.test.js
@@ -1,47 +1,43 @@
-'use strict';
-const request = require('request');
-const chai = require('chai');
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-describe('basic integration testing', () => {
-  describe('GET /', () => {
-    it('endpoint: GET /', (done) => {
-      const call = {
-        url: 'http://localhost:7865',
-        method: 'GET',
-      };
-      request(call, (error, response, body) => {
-        chai.expect(response.statusCode).to.equal(200);
-        chai.expect(body).to.equal('Welcome to the payment system');
-        done();
-      });
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
     });
-  });
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.equal("Welcome to the payment system");
+	    done();
+	});
+    });
 });
 
-describe('regex integration testing', () => {
-  describe('GET /cart/:id', () => {
-    it('endpoint: GET /cart/:id', (done) => {
-      const call = {
-        url: 'http://localhost:7865/cart/12',
-        method: 'GET',
-      };
-      request(call, (error, response, body) => {
-        chai.expect(response.statusCode).to.equal(200);
-        chai.expect(body).to.equal('Payment methods for cart 12');
-        done();
-      });
+describe("Cart page", function() {
+    it("check correct status code for correct url", function(done) {
+	request.get("http://localhost:7865/cart/12", function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
     });
-  });
-  describe('GET /cart/:isNaN', () => {
-    it('endpoint: GET /cart/:isNaN', (done) => {
-      const call = {
-        url: 'http://localhost:7865/cart/anything',
-        method: 'GET',
-      };
-      request(call, (error, response, body) => {
-        chai.expect(response.statusCode).to.equal(404);
-        done();
-      });
+    it("check correct content for correct url", function(done) {
+	request.get("http://localhost:7865/cart/12", function(err, res, body) {
+	    expect(body).to.contain("Payment methods for cart 12");
+	    done();
+	});
     });
-  });
+    it("check correct status code for incorrect url", function(done) {
+	request.get("http://localhost:7865/cart/kim", function(err, res, body) {
+	    expect(res.statusCode).to.equal(404);
+	    done();
+	});
+    });
 });
